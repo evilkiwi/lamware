d29f973b0bd45e73b59ef7c6fcaef08ad6f218d8
OYED <tom@mosey.io>
2022-03-25 08:04:57 +0800

    feat(memoize): add new middleware

---
+++ packages/memoize/src/index.ts
@@ -0,0 +1,20 @@
+import type { Middleware } from '@tnotifier/lamware';
+import type { Handler } from 'aws-lambda';
+
+export interface Options {
+    throwOnError?: boolean;
+}
+
+export const memoize = <S extends object>(closure: () => Promise<Partial<S>|void>, options?: Options): Middleware<Handler, S> => ({
+    id: 'memoize',
+    pure: true,
+    init: async () => {
+        try {
+            return await closure();
+        } catch (e) {
+            if (options?.throwOnError !== false) {
+                throw new Error(`failed to memoize: ${e}`);
+            }
+        }
+    },
+});
