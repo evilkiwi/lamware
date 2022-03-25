8eaafbc7eaec06b6ceace800cbca7e18aba1f1a9
OYED <tom@mosey.io>
2022-03-26 06:05:38 +0800

    chore(do-not-wait): add testing

---
+++ packages/do-not-wait/test/do-not-wait.test.ts
@@ -0,0 +1,23 @@
+import type { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import { lamware, clearMiddleware } from '@lamware/core';
+import { afterEach, expect, test } from 'vitest';
+import { execute } from '@lamware/test';
+import { doNotWait } from '../src';
+
+afterEach(() => clearMiddleware());
+
+test('should set callbackWaitsForEmptyEventLoop to false when used', async () => {
+    let callbackValue: boolean|null = null;
+
+    const { handler } = lamware<APIGatewayProxyHandlerV2<any>>()
+        .use(doNotWait())
+        .execute(async ({ context }) => {
+            callbackValue = context.callbackWaitsForEmptyEventLoop;
+
+            return { statusCode: 200 };
+        });
+    const result = await execute(handler);
+
+    expect(result.statusCode).toBe(200);
+    expect(callbackValue).toBe(false);
+});
